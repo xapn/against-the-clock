9286561d39e4390ae1487bf303c195bbb434c8c5
Xavier Pigeon <xpn.projects@gmail.com>
2018-08-02 17:35:29 +0200

    Refactor tests to have definitive test cases.

--- src/test/java/AgainstTheClockTest.java
+++ src/test/java/AgainstTheClockTest.java
@@ -1,34 +1,33 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import testasyouthink.GivenWhenThenDsl.PreparationStage.Given;
 
 import static testasyouthink.TestAsYouThink.givenSutClass;
 
 class AgainstTheClockTest {
 
-    @Nested
-    @DisplayName("Characterization tests")
-    class CharacterizationTesting {
+    private Given<AgainstTheClock> givenAgainstTheClockAsSut;
 
-        @Test
-        void cover_at_100_percent() {
-            givenSutClass(AgainstTheClock.class)
-                    .givenArgument(() -> new AutonomousCar(new Battery()))
-                    .whenSutRunsOutsideOperatingConditions((sut, car) -> sut.start(car, 99))
-                    .thenItFails();
-        }
+    @BeforeEach
+    void prepareFixtures() {
+        givenAgainstTheClockAsSut = givenSutClass(AgainstTheClock.class);
     }
 
-    @Nested
-    @DisplayName("Fixes")
-    class DefectBasedTesting {
+    @Test
+    void should_start_the_automatic_driving_of_an_autonomous_car() {
+        givenAgainstTheClockAsSut
+                .givenArgument(() -> new AutonomousCar(new Battery()))
+                .whenSutRuns((sut, car) -> sut.start(car, 100))
+                .thenItSucceeds();
+    }
 
-        @Test
-        void should_fix_the_fuel_tank_filling() {
-            givenSutClass(AgainstTheClock.class)
-                    .givenArgument(() -> new AutonomousCar(new Battery()))
-                    .whenSutRuns((sut, car) -> sut.start(car, 100))
-                    .thenItSucceeds();
-        }
+    @Test
+    void should_fail_to_prepare_the_autonomous_car_given_a_lack_of_fuel() {
+        givenAgainstTheClockAsSut
+                .givenArgument(() -> new AutonomousCar(new Battery()))
+                .whenSutRunsOutsideOperatingConditions((sut, car) -> sut.start(car, 99))
+                .thenItFails()
+                .becauseOf(IllegalStateException.class)
+                .withMessage("Can't start an autonomous car without having filled up the fuel tank capacity");
     }
 }
